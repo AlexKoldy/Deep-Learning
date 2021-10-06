22090aad038a2e2ff2b491008566d82ee3258c9f
AlexKoldy <koldy@cooper.edu>
2021-10-06 17:03:22 -0400

    update assignment4

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -14,8 +14,8 @@ class Data():
 	def __init__(self, files, is_training):
 		'''Training, validation, and testing data parameters'''
 		self.image_shape = (32, 32)
-		self.num_training = 40000
-		self.num_validation = 10000
+		self.num_training = 45000
+		self.num_validation = 5000
 		self.num_testing = 10000
 
 		self.load_data(files, is_training)
@@ -39,47 +39,49 @@ class Data():
 
 		if is_training:		
 			'''Establish training and validation data'''
+			new_order = np.random.permutation(self.num_training + self.num_validation)
+			self.training_data = self.training_data[new_order, :, :, :]
 			self.validation_data = self.training_data[self.num_training:, :, :, :]
 			self.training_data = self.training_data[:self.num_training, :, :, :]
-			#new_order = np.random.permutation(self.num_training)
-			#self.training_data = self.training_data[new_order, :, :, :]
-
-
+			
 			'''Establish training and validation labels'''
+			self.training_labels = self.training_labels[new_order]
 			self.validation_labels = self.training_labels[self.num_training:].flatten()
 			self.training_labels = self.training_labels[:self.num_training].flatten()
-			#self.training_labels = self.training_labels[new_order]
 			
 			'''Normalize'''
-			#self.training_data = self.training_data.astype('float32') / 255.0
+			self.training_data = self.training_data.astype('float32') / 255.0
 			self.training_data = self.training_data / 255.0
-			#self.validation_data = self.validation_data.astype('float32') / 255.0
+			self.validation_data = self.validation_data.astype('float32') / 255.0
 			self.validation_data = self.validation_data / 255.0
 
 			width_shift = 3/32
 			height_shift = 3/32
 			flip = True
-
 			self.datagen = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=flip, width_shift_range=width_shift, height_shift_range=height_shift)
 			self.datagen.fit(self.training_data)
 			
-			'''
-			def visualize_data(images):
-				fig = plt.figure(figsize=(14, 6))
-				fig.patch.set_facecolor('white')
-				for i in range(3 * 7):
-					plt.subplot(3, 7, i+1)
-					plt.xticks([])
-					plt.yticks([])
-					plt.imshow(images[i])
-				plt.show()
 			
-
-			it = self.datagen.flow(self.training_data, self.training_labels, shuffle=False)
-			batch_images, batch_labels = next(it)
-			visualize_data(batch_images)
-			'''	
-
+			print("0")
+			print(np.count_nonzero(self.validation_labels == 0))
+			print("1")
+			print(np.count_nonzero(self.validation_labels == 1))
+			print("2")
+			print(np.count_nonzero(self.validation_labels == 2))
+			print("3")
+			print(np.count_nonzero(self.validation_labels == 3))
+			print("4")
+			print(np.count_nonzero(self.validation_labels == 4))
+			print("5")
+			print(np.count_nonzero(self.validation_labels == 5))
+			print("6")
+			print(np.count_nonzero(self.validation_labels == 6))
+			print("7")
+			print(np.count_nonzero(self.validation_labels == 7))
+			print("8")
+			print(np.count_nonzero(self.validation_labels == 8))
+			print("9")
+			print(np.count_nonzero(self.validation_labels == 9))
 			
 			
 		else:
@@ -96,8 +98,6 @@ class Data():
 			plt.imshow(img) 
 			plt.show()
 
-		
-		
 	def unpickle(self, file):
 		import pickle
 		with open(file, 'rb') as f:
@@ -108,54 +108,24 @@ class ResNet():
 	def __init__(self):
 		self.num_classes = 10
 		self.input = tf.keras.Input(shape=(32, 32, 3))
-		
-		
-	
 		x =  tf.keras.layers.ZeroPadding2D(padding=(3, 3))(self.input)
-		x = self.convolution_block(x, 1, 32)
-		x = self.identity_block(x, 32)
-		#x = tf.keras.layers.MaxPool2D((2, 2))
-
-		x = self.convolution_block(x, 1, 64)
-		x = self.identity_block(x, 64)
-		#x = tf.keras.layers.MaxPool2D((2, 2))
-
-		x = self.convolution_block(x, 1, 128)
-		x = self.identity_block(x, 128)
-		#x = tf.keras.layers.MaxPool2D((2, 2))
-
-		
-
-		'''
-		x = tf.keras.layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2))(x)
+	
+		x = tf.keras.layers.Conv2D(16, kernel_size=(3, 3), strides=(2, 2))(x)
 		x = tf.keras.layers.BatchNormalization()(x)
 		x = tf.keras.layers.Activation('relu')(x)
 		x = tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
 
-		x = self.convolution_block(x, 1, 64)
-		x = self.identity_block(x, 64)
-		x = self.identity_block(x, 64)
+		x = self.convolution_block(x, 1, 16)
+		x = self.identity_block(x, 16)
 
-		x = self.convolution_block(x, 2, 128)
-		x = self.identity_block(x, 128)
-		x = self.identity_block(x, 128)
-		x = self.identity_block(x, 128)
+		x = self.convolution_block(x, 2, 32)
+		x = self.identity_block(x, 32)
 		
-		x = self.convolution_block(x, 2, 256)
-		x = self.identity_block(x, 256)
-		x = self.identity_block(x, 256)
-		x = self.identity_block(x, 256)
-		x = self.identity_block(x, 256)
-		x = self.identity_block(x, 256)
-		
-		x = self.convolution_block(x, 2, 512)
-		x = self.identity_block(x, 512)
-		x = self.identity_block(x, 512)
-		'''
-		
+		x = self.convolution_block(x, 2, 64)
+		x = self.identity_block(x, 64)
+				
 		x = tf.keras.layers.AveragePooling2D((2, 2), padding='same')(x)
 		x = tf.keras.layers.Flatten()(x)
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
 		self.output = tf.keras.layers.Dense(self.num_classes, activation='softmax', kernel_initializer='he_normal')(x)
 
 		self.model = tf.keras.Model(inputs=self.input, outputs=self.output)
@@ -206,125 +176,32 @@ class ResNet():
 
 		return x
 
+def try_resnet():
+	training_files = ['data/cifar-10-python/cifar-10-batches-py/data_batch_1', 
+			'data/cifar-10-python/cifar-10-batches-py/data_batch_2',
+			'data/cifar-10-python/cifar-10-batches-py/data_batch_3',
+			'data/cifar-10-python/cifar-10-batches-py/data_batch_4',
+			'data/cifar-10-python/cifar-10-batches-py/data_batch_5']
+	data = Data(training_files, True)
 
+	'''Establish Convolutional Neural Network'''
+	resnet = ResNet()
+	model = resnet()
 
+	model.summary()
 
-training_files = ['data/cifar-10-python/cifar-10-batches-py/data_batch_1', 
-		 'data/cifar-10-python/cifar-10-batches-py/data_batch_2',
-		 'data/cifar-10-python/cifar-10-batches-py/data_batch_3',
-		 'data/cifar-10-python/cifar-10-batches-py/data_batch_4',
-		 'data/cifar-10-python/cifar-10-batches-py/data_batch_5']
-data = Data(training_files, True)
+	'''Training/Validation'''
+	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy', 'sparse_top_k_categorical_accuracy'])
 
-'''Establish Convolutional Neural Network'''
-'''
-model = tf.keras.models.Sequential()
+	print("Training/Validation Data!")
+	history = model.fit(data.datagen.flow(data.training_data, data.training_labels, batch_size=64), epochs=160, validation_data=(data.validation_data, data.validation_labels),)# verbose=2)
 
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01),  input_shape=(32, 32, 3)))
-model.add(tf.keras.layers.BatchNormalization())
+	'''Testing'''
+	testing_files = ['data/cifar-10-python/cifar-10-batches-py/test_batch']
+	data = Data(testing_files, False)
 
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-model.add(tf.keras.layers.Dropout(0.3))
-
-model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-
-model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-model.add(tf.keras.layers.Dropout(0.3))
-
-model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-
-model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-model.add(tf.keras.layers.Dropout(0.3))
-model.add(tf.keras.layers.Flatten())
-
-model.add(tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.BatchNormalization())
-
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
-'''
-
-resnet = ResNet()
-model = resnet()
-
-model.summary()
-
-'''Training/Validation'''
-model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy', 'sparse_top_k_categorical_accuracy'])
-
-print("Training/Validation Data!")
-history = model.fit(data.datagen.flow(data.training_data, data.training_labels, batch_size=64), epochs=200, validation_data=(data.validation_data, data.validation_labels))
-
-'''Testing'''
-testing_files = ['data/cifar-10-python/cifar-10-batches-py/test_batch']
-data = Data(testing_files, False)
-
-print("Testing Data!")
-history = model.evaluate(data.testing_data, data.testing_labels)
-
-'''
-def create_model():
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(tf.keras.layers.MaxPool2D((2,2)))
-
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same',))
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same',))
-    model.add(tf.keras.layers.MaxPool2D((2,2)))
-
-    model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same',))
-    model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same',))
-    model.add(tf.keras.layers.MaxPool2D((2,2)))
-
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    return model
-
-
-batch_size = 32
-epochs = 50
-m_no_aug = create_model()
-m_no_aug.summary()
-
-history_no_aug = m_no_aug.fit(
-    data.training_data, data.training_labels,
-    epochs=epochs, batch_size=batch_size,
-    validation_data=(data.validation_data, data.validation_labels))
-
-data = Data(testing_files, False)
-m_no_aug.evaluate(data.testing_data,  data.testing_labels)
-'''
-
+	print("Testing Data!")
+	history = model.evaluate(data.testing_data, data.testing_labels)
 
 '''
 References:
